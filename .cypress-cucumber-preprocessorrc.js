611ddb4e34c626c79e9b9b634a83a8735f21e5fd
Gregor Adams <greg@pixelass.com>
2022-04-23 15:34:16 +0200

    chore: adjust config files
    use *rc.json where possible
    fixes ncurc logic

--- .cypress-cucumber-preprocessorrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	nonGlobalStepDefinitions: true,
-	cucumberJson: {
-		generate: true,
-		outputFolder: "cypress/cucumber-json",
-		filePrefix: "",
-		fileSuffix: ".cucumber",
-	},
-};
