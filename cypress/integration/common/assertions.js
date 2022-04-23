c75dd59a099c44eb203c438ca522f65d55e8a539
Gregor Adams <greg@pixelass.com>
2022-04-23 15:34:40 +0200

    test: adjust demo test file

--- cypress/integration/common/assertions.js
+++ cypress/integration/common/assertions.js
@@ -1,5 +1,4 @@
 import { Then } from "cypress-cucumber-preprocessor/steps";
-import { dataTestId } from "../helpers";
 
 Then(/^the page is displayed$/, function () {
 	cy.get("#__next").should("exist");
